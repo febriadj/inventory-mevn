a94846e4b7bbc3ee50d83a9310ed6d38648660f9
febriadj <iamfebriadji@gmail.com>
2021-10-13 23:50:51 +0700

    :hammer: Add apollo provider

--- client/index.js
+++ client/index.js
@@ -1,18 +1,31 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Fragment from 'vue-fragment';
+import ApolloClient from 'apollo-boost';
+import VueApollo from 'vue-apollo';
 
 import router from './router/index';
 import App from './app.vue';
 
 Vue.use(VueRouter);
 Vue.use(Fragment.Plugin);
+Vue.use(VueApollo);
 
 Vue.config.errorHandler = (err) => {
   console.log(err.message);
 };
 
+const isDev = process.env.NODE_ENV === 'development';
+const apolloClient = new ApolloClient({
+  uri: isDev ? 'http://localhost:8000/graphql' : '/graphql',
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
 new Vue({
-  render: (element) => element(App),
+  apolloProvider,
   router,
+  render: (element) => element(App),
 }).$mount('#root');
