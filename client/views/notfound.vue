8b882d9942d56e509a0327707a5ea4e9bb43f087
febriadj <iamfebriadji@gmail.com>
2021-10-17 15:33:33 +0700

    :hammer: Update notfound view

--- client/views/notfound.vue
+++ client/views/notfound.vue
@@ -1,5 +1,16 @@
 <template>
-  <p class="paragraf">Not Found Page</p>
+  <div :id="$style.notfound">
+    <div :id="$style['notfound-wrap']">
+      <h1 :class="$style.title">Oops. 404</h1>
+      <p :class="$style.paragraf">The page you are looking for was not found</p>
+      <div :class="$style.toggle">
+        <router-link to="/" :class="$style.btn">
+          <i class="bx bx-undo"></i>
+          <p>To Dashboard</p>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,8 +19,27 @@ export default {
 }
 </script>
 
-<style scoped>
-.paragraf {
-  text-decoration: underline;
+<style module>
+#notfound {
+  position: absolute;
+  width: 100%; height: 100%;
+  display: flex; justify-content: center; align-items: center;
+  background: #fcf7ff;
+  z-index: 9;
+}
+.title {
+  text-transform: uppercase;
+}
+.toggle {
+  display: flex;
+  margin: 20px 0 0 0;
+}
+.toggle .btn {
+  display: flex; align-items: center; gap: 10px;
+  text-decoration: none;
+  background: #95899e;
+  padding: 5px 15px 5px 10px;
+  color: #fff;
+  border-radius: 2rem;
 }
 </style>
